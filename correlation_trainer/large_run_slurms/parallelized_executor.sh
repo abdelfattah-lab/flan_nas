7ac369b541a0fb2961961c6beba1c62c90c5eac0
Yash Akhauri <ya255@abdelfattah-compute-01.tech.cornell.edu>
2023-09-19 20:03:14 -0400

    results, nas_prep

--- correlation_trainer/large_run_slurms/parallelized_executor.sh
+++ correlation_trainer/large_run_slurms/parallelized_executor.sh
@@ -28,7 +28,6 @@ while IFS= read -r line; do
 #SBATCH --mem=$mem
 #SBATCH -t 16:00:00
 #SBATCH --account=abdelfattah
-#SBATCH --partition=abdelfattah
 EOL
 
         # Conditionally add the GPU line
